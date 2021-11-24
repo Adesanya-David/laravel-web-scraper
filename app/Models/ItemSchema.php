b7d74f084c1579662dbdfade1501df2d97ed4c9b
Adesanya David <tito@mytransita.com>
2021-11-24 14:54:58 +0100

    Initial commit

---
+++ app/Models/ItemSchema.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ItemSchema extends Model
+{
+protected $table = "item_schema";
+    use HasFactory;
+}
